f0d7c342c541836c89eafbe6b7dd5223c468dd2e
buddymajki <buddymajki@gmail.com>
2026-02-16 12:08:13 +0100

    update javítások

--- admin/flutter_service_worker.js
+++ admin/flutter_service_worker.js
@@ -27,14 +27,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "b63899219c17957a9e921cb8ea79d126
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "ba77ce5f5140aa946c37d47886a2f752",
+"flutter_bootstrap.js": "8d074bbc1502307fff1ad43ba48c0246",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "0dc31b1db1e0b225a07bc154063e3b6d",
-"/": "0dc31b1db1e0b225a07bc154063e3b6d",
-"main.dart.js": "39919e1f676102dec61bd49d7c7998c6",
+"index.html": "85942f79cc8b5761a0c30e71d76bae61",
+"/": "85942f79cc8b5761a0c30e71d76bae61",
+"main.dart.js": "b08e0d8456f1c3d5ec78fe8189c1f295",
 "manifest.json": "4a8473265bad5fa589c37cc7e4d68b55",
 "version.json": "87f2ee378aa8ef1b0c55b3285c39b20e"};
 // The application shell files that are downloaded before a service worker can
